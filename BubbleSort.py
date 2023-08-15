683e1c5faa1c674b101ca220efab7cc1e014c530
shehrozali12 <dj23945@gmail.com>
2023-08-15 18:21:11 +0500

    Second Commit

---
+++ BubbleSort.py
@@ -0,0 +1,33 @@
+# Bubble sort in Python
+
+def bubbleSort(array):
+    
+  # loop to access each array element
+  for i in range(len(array)):
+
+    # loop to compare array elements
+    for j in range(0, len(array) - i - 1):
+
+      # compare two adjacent elements
+      # change > to < to sort in descending order
+      if array[j] > array[j + 1]:
+
+        # swapping elements if elements
+        # are not in the intended order
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+
+
+#data = [-2, 45, 0, 11, -9]
+
+data=[]
+n=int(input("Number of elements in array:"))
+for i in range(0,n):
+   l=int(input())
+   data.append(l)
+
+bubbleSort(data)
+
+print('Sorted Array in Ascending Order:')
+print(data)
